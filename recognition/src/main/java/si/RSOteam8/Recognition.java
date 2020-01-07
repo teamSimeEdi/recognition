995a3715400dfa51f8cab91fc0fa2450b3bbbf89
SimeZa <32951221+SimeZa@users.noreply.github.com>
2020-01-07 22:46:00 +0100

    k fixed

---
+++ recognition/src/main/java/si/RSOteam8/Recognition.java
@@ -0,0 +1,23 @@
+package si.RSOteam8;
+
+public class Recognition {
+    private String confidence;
+    private String tag;
+
+    public String getConfidence() {
+        return confidence;
+    }
+
+    public void setConfidence(String confidence) {
+        this.confidence = confidence;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+}
